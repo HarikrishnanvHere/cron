1cc61eaaa519630f1ea0e42d73e0138a21db7134
HarikrishnanHere <itsmeharikrishnanv@gmail.com>
2025-01-21 13:03:32 +0530

    sending mail through nodemailer using cronjob every 15 minutes.

---
+++ index.ts
@@ -0,0 +1,44 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+import cron from "node-cron";
+import nodemailer from "nodemailer";
+import { text } from "stream/consumers";
+
+//implementing mail sending functionality
+
+function sendMailThroughCron() {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "harikrishnantestmail@gmail.com",
+      pass: "yghi kkom genq odvc",
+    },
+  });
+
+  const mailOptions = {
+    from: "harikrishnantestmail@gmail.com",
+    to: "harikrishnanv.rko@gmail.com",
+    subject: "Sending Email using Node.js",
+    text: "If this message finds you, the mail is working fine",
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
+
+//Scheduling a cronjob
+
+cron.schedule("*/30 * * * * *", () => {
+  sendMailThroughCron();
+  console.log("rsending a mail every 30 seconds");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
